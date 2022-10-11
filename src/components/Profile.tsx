f1e105f69bc0cca52c266b0df526e421c7c061e2
Albert <bert21387@gmail.com>
2022-10-11 21:50:22 +0500

    Создал css модули

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVI0Cqa5bYHtHDE5-fVDxVeGtZV6anu8qVzw&usqp=CAU" alt="image"/>
             </div>
@@ -17,10 +18,10 @@ const Profile = () => {
                 <div>
                     Post1
                 </div>
-                <div>
+                <div className={s.item}>
                     Post2
                 </div>
-                <div>
+                <div className={s.item}>
                     Post3
                 </div>
             </div>
