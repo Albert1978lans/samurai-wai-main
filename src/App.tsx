4901d8abc07138fe95a4fac98c27f4c77eab82d8
Albert <bert21387@gmail.com>
2024-05-16 20:47:35 +0500

    95 - ReactJS + github pages, разворачиваем наш проект deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
@@ -9,19 +9,19 @@ import {Settings} from "./components/Settings/Settings";
 
 
 import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginPage from "./components/Login/Login";
+
 import {compose} from "redux";
 import {connect} from "react-redux";
 import {AppStateType} from "./redux/redux-store";
 import {initializeApp} from "./redux/app-reducer";
 import {Preloader} from "./components/common/Preloader/Preloader";
+import {withSuspense} from "./hok/withSuspense";
 
-// import UsersContainer from "./components/Users/Users.Container";
-// import ProfileContainer from "./components/Profile/ProfileContainer";
-// import DialogsContainer from "./components/Dialogs/DialogsContainer";
+
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const UsersContainer = React.lazy(() => import('./components/Users/Users.Container'));
+const LoginPage = React.lazy(() => import('./components/Login/Login'));
 
 
 class App extends React.Component<mapStateToPropsType & mapDispatchToPropsType> {
@@ -37,33 +37,28 @@ class App extends React.Component<mapStateToPropsType & mapDispatchToPropsType> 
         }
 
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <div className="app-wrapper">
                     <HeaderContainer/>
                     <Navbar/>
                     <div className='app-wrapper-content'>
                         <Route path={'/profile/:userId?'}  render={() => {
-                            return <Suspense fallback={<Preloader/>}>
-                                <ProfileContainer/>
-                            </Suspense>
-                        }}/>
+                            return <Suspense fallback={<Preloader/>}><ProfileContainer/></Suspense>                        }}/>
                         <Route path={'/dialogs'} render={() => {
-                            return <Suspense fallback={<Preloader/>}>
-                                <DialogsContainer/>
-                            </Suspense>
+                            return <Suspense fallback={<Preloader/>}><DialogsContainer/></Suspense>
                         }}/>
                         <Route path={'/users'} render={() => {
-                            return <Suspense fallback={<Preloader/>}>
-                                <UsersContainer/>
-                            </Suspense>
+                            return <Suspense fallback={<Preloader/>}><UsersContainer/></Suspense>
                         }}/>
-                        <Route path={'/login'} render={() => <LoginPage/>}/>
+                        <Route path={'/login'} render={() => {
+                            return <Suspense fallback={<Preloader/>}><LoginPage/></Suspense>
+                        }}/>
                         <Route path={'/news'} component={News}/>
                         <Route path={'/music'} component={Music}/>
                         <Route path={'/settings'} component={Settings}/>
                     </div>
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         );
     }
 }
